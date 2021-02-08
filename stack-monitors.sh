be280ac5d1b748b52707a49bcce37f2deb48ca8c
Yashwanth Allakky <yashwanth.allakky@codingstudio.club>
2021-02-08 23:08:52 +0530

    Create stack-monitors.sh

---
+++ stack-monitors.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output DP-0 --off --output DP-1 --off --output HDMI-0 --mode 1366x768 --pos 277x0 --rotate normal --output DP-2 --off --output DP-3 --off --output eDP-1-1 --mode 1920x1080 --pos 0x768 --rotate normal
